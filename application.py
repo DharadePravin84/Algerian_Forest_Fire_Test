0df82de60e93c3003499e24caeb0d812ef0078da
DharadePravin84 <dharadepravin84@gmail.com>
2024-03-05 17:26:55 +0530

    Beanstalk configuration

--- application.py
+++ application.py
@@ -1,24 +1,24 @@
 import pickle
+from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-from flask import Flask,request,jsonify,render_template
-
 application = Flask(__name__)
 app=application
 
-## import ridge regressor and standard scaler pickle
+## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
-@app.route("/")
+## Route for home page
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method=="POST":
+    if request.method=='POST':
         Temperature=float(request.form.get('Temperature'))
         RH = float(request.form.get('RH'))
         Ws = float(request.form.get('Ws'))
@@ -32,8 +32,8 @@ def predict_datapoint():
         new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
         result=ridge_model.predict(new_data_scaled)
 
-        return render_template('home.html',results=result[0])
-        
+        return render_template('home.html',result=result[0])
+
     else:
         return render_template('home.html')
 
